43ab3b31b6cef3195fccf70ad944025a7170b528
Ульяна <skipina_ulyana24@mail.ru>
2023-10-09 00:06:06 +0300

    work

--- src/main/java/ru/volkova/array2app/_main/_Main.java
+++ src/main/java/ru/volkova/array2app/_main/_Main.java
@@ -2,16 +2,15 @@ package ru.volkova.array2app._main;
 
 import ru.volkova.array2app.service.Service;
 
+import java.util.Arrays;
+
 public class _Main {
 
     public static void main(String [] args) {
-        int [] arr2 = new int[4];
-        arr2[0]= 6;
-        arr2[1]= -2;
-        arr2[2]= 15;
-        arr2[3]= 4;
+        int [] arr2 = {6, -2, 15, 4};
 
         Service service = new Service();
         service.ascending(arr2);
+        System.out.println(Arrays.toString(arr2));
     }
 }
--- src/main/java/ru/volkova/array2app/service/Service.java
+++ src/main/java/ru/volkova/array2app/service/Service.java
@@ -4,20 +4,20 @@ public class Service {
 
     public void ascending(int [] arr2) {
             int index = 0;
-            while (index < 4) {
+            while (index < arr2.length - 1) {
                 int num1 = arr2[index];
                 int num2 = arr2[index + 1];
+                int i = index;
+                int j = index + 1;
                 if (num1<num2) {
                     index++;
                 } else {
-                    num1 = arr2[index + 1];
-                    num2 = arr2[index - 1];
-                    if (index < 0){
-                        index = 0;
-                    }
-                    index++;
+                    int temp = arr2[i];
+                    arr2[i] = arr2[j];
+                    arr2[j] = temp;
+                    i++;
+                    j--;
                 }
             }
-            System.out.println(arr2);
         }
     }
